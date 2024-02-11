956dc51dbda5ef2af087530cc02ca87e6ab92b4d
Hector Puga <pugagarciah@gmail.com>
2024-02-11 09:05:13 -0600

    Primer programa Hola Mundo, Tarea MiNombre

---
+++ ImprimerNombre/ImprimerNombre/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImprimerNombre
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine("Héctor Puga");
+            Console.WriteLine("Me gusta programar");
+            Console.WriteLine("Me gusta la musica");
+		}
+	}
+}
